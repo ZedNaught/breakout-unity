0057e36b86bc8bbf636a6b029e17235c5a3f4575
Alexander Reyes <alex@ridgebox.com>
2016-08-23 17:51:32 -0600

    set up walls and launching; foolin

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -4,13 +4,51 @@ using System.Collections;
 public class BallMovement : MonoBehaviour {
 
     public float ballSpeed;
+    public GameObject bat;
 
     private Rigidbody2D rb;
+    private bool onBat;
+    private float ballRadius;
+    private float verticalBatOffset;
 
-	// Use this for initialization
-	void Start () {
-	    rb = GetComponent<Rigidbody2D>();
-        rb.velocity = ballSpeed * (new Vector2(1.0f, -2.0f)).normalized;
-        Debug.Log(rb.velocity);
-	}
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        ballRadius = GetComponent<CircleCollider2D>().radius;
+        verticalBatOffset = ballRadius + bat.GetComponent<BoxCollider2D>().size.y / 2.0f + 0.1f;
+
+        ResetToBat();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown("space") && onBat) {
+            Launch();
+        }
+        if (onBat) {
+            TrackBat();
+        }
+    }
+
+    void StartMovement() {
+        rb.velocity = ballSpeed * (new Vector2(-1.0f, 2.0f)).normalized;
+    }
+
+    void ResetToBat() {
+        onBat = true;
+        rb.isKinematic = true;
+    }
+
+    void Launch() {
+        rb.isKinematic = false;
+        onBat = false;
+        StartMovement();
+    }
+
+    void TrackBat() {
+        transform.position = new Vector2(bat.transform.position.x, bat.transform.position.y + verticalBatOffset);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log(other);
+    }
+
 }
