68db4ffd2b4820a6f982093985672aa3a107f520
Alexander Reyes <alex@ridgebox.com>
2016-08-22 18:58:14 -0600

    add sprites preliminary scripts and scene setup change line endings in some "standard assets" scripts

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : MonoBehaviour {
+
+    public float ballSpeed;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+	    rb = GetComponent<Rigidbody2D>();
+        rb.velocity = ballSpeed * (new Vector2(1.0f, -2.0f)).normalized;
+        Debug.Log(rb.velocity);
+	}
+}
---
+++ Assets/Scripts/BatMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatMovement : MonoBehaviour {
+    public float batSpeed;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+	    rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	    float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(batSpeed * xInput, 0);
+	}
+}
